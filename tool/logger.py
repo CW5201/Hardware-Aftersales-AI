2a092adb83f29401961409bd6bd43d043cca6140
CW5201 <19317334773@163.com>
2026-01-30 10:00:00 +0800

    add logger

---
+++ tool/logger.py
@@ -0,0 +1,30 @@
+# 默认名字是：root
+import logging
+import os
+
+import colorlog
+
+LOG_LEVEL = os.getenv("LOG_LEVEL", "DEBUG")
+
+logger = logging.getLogger()
+logger.setLevel(getattr(logging, LOG_LEVEL, logging.DEBUG))
+
+# 加载彩色日志处理器
+handler = colorlog.StreamHandler()
+# 定义日志输出的格式
+handler.setFormatter(colorlog.ColoredFormatter(
+    '%(log_color)s%(asctime)s - %(filename)s : %(lineno)d - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    log_colors={
+        'DEBUG': 'cyan',
+        'INFO': 'green',  # INFO 显示为绿色
+        'WARNING': 'yellow',
+        'ERROR': 'red',
+        'CRITICAL': 'bold_red',
+    }
+))
+
+# logger.handlers.clear()
+
+# 应用日志配置信息
+logger.addHandler(handler)
