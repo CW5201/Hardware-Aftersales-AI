1fa67f0f7cb92a37bceba2c7d543b1f62f99570e
CW5201 <19317334773@163.com>
2026-05-02 10:00:00 +0800

    fix: 修复并发导入Milvus写入冲突

--- processor/import_processor/nodes/node_import_milvus.py
+++ processor/import_processor/nodes/node_import_milvus.py
@@ -247,7 +247,7 @@ import threading
 _milvus_write_lock = threading.Lock()
 
 def safe_milvus_insert(client, collection_name, data):
-    """线程安全的Milvus插入操作"""
+    """修复并发导入Milvus写入冲突"""
     with _milvus_write_lock:
         try:
             client.insert(collection_name=collection_name, data=data)
