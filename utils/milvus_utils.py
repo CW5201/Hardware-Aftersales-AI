913a36ba48e83da69e4c525e18ec8bf0dd758093
CW5201 <19317334773@163.com>
2026-05-19 10:00:00 +0800

    optimize: 优化Milvus批量写入

--- utils/milvus_utils.py
+++ utils/milvus_utils.py
@@ -143,7 +143,7 @@ MILVUS_BATCH_SIZE = 100
 MILVUS_BATCH_MAX_MEMORY = 10000
 
 def batch_insert(client, collection_name, data):
-    """修复Milvus批量写入内存溢出"""
+    """优化Milvus批量写入"""
     if not data:
         return 0
     total = 0
